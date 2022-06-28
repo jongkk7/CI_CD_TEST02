a2a2bc0901b06a93c7c41299ad1acf5255890f7c
yjk <a01089929866@gmail.com>
2022-06-28 15:19:51 +0900

    ci test

--- app/src/main/java/com/yjk/ci_cd_test02/MainActivity.kt
+++ app/src/main/java/com/yjk/ci_cd_test02/MainActivity.kt
@@ -2,10 +2,19 @@ package com.yjk.ci_cd_test02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+
+// CI Test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test()
+    }
+
+    fun test(){
+        Log.d("###", "test()..")
     }
 }
